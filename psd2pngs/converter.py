73d2d9d01d9f423b394682bd5b45917f157d7753
34j <55338215+34j@users.noreply.github.com>
2022-08-14 19:26:50 +0900

    Changed option names and added --json option

--- psd2pngs/converter.py
+++ psd2pngs/converter.py
@@ -1,3 +1,4 @@
+from ntpath import join
 from typing import Optional
 from psd_tools import PSDImage
 from pathlib import Path
@@ -8,7 +9,8 @@ from logging import StreamHandler, getLogger, DEBUG
 import numpy as np
 import multiprocessing
 from psd2pngs.version import __version__
-from psd2pngs.layers import save_some_layers, search_all_layers, save_layer
+from psd2pngs.layers import save_some_layers, search_all_layers, save_layer, get_layer_info
+import json
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'])
@@ -17,13 +19,14 @@ CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.version_option(__version__, '-v', '--version', prog_name='psd2pngs')
 @click.argument('psd_path', type=click.Path(exists=True))
-@click.option('--out-dir-path', '-o', type=click.Path(exists=True), default=None,
+@click.option('--out', '-o', 'out_dir_path', type=click.Path(exists=True), default=None,
               help='Output directory path. If not specified, output to the same directory as the PSD file.')
 @click.option('--single-process', '-s', is_flag=True,
               help='Force not to use multiprocessing.')
-@click.option('--tasks-count', '-t', type=int, default=multiprocessing.cpu_count(),
+@click.option('--tasks-count', '-t', 'n_tasks', type=int, default=multiprocessing.cpu_count(),
               help=f'Number of tasks. Recommended to be less than or equal to the number of CPUs ({multiprocessing.cpu_count()}) because the process maximizes the use of CPUs.')
-def psd2pngs(psd_path: str, out_dir_path: Optional[str] = None, single_process: bool = False, tasks_count=multiprocessing.cpu_count()):
+@click.option('--json', '-j','use_json', is_flag=True, help='Output JSON file containing layer information.', )
+def psd2pngs(psd_path: str, out_dir_path: Optional[str] = None, single_process: bool = False, n_tasks=multiprocessing.cpu_count(), use_json: bool = False):
     psd_path_ = Path(psd_path).absolute()
     out_dir_path_ = psd_path_.parent
     if out_dir_path is not None:
@@ -41,13 +44,19 @@ def psd2pngs(psd_path: str, out_dir_path: Optional[str] = None, single_process: 
     logger.setLevel(DEBUG)
 
     # validate values
-    if tasks_count > multiprocessing.cpu_count():
+    if n_tasks > multiprocessing.cpu_count():
         logger.warning('--tasks-count is larger than the number of CPUs.')
 
     # search all layers
     logger.info('Searching all layers...')
     all_layers = list(tqdm(search_all_layers(
-        psd, out_dir_path_), unit=' layer(s)'))
+        psd, out_dir_path_), unit=' layer(s) found'))
+
+    if use_json:
+        logger.info('Saving JSON file...')
+        json_path = out_dir_path_.joinpath(psd_path_.stem + '.json')
+        with open(json_path, 'w') as f:
+            json.dump(get_layer_info(psd), f, indent=4, ensure_ascii=False)
 
     # save layers
     logger.info('Saving layers...')
@@ -57,8 +66,8 @@ def psd2pngs(psd_path: str, out_dir_path: Optional[str] = None, single_process: 
         with concurrent.futures.ProcessPoolExecutor() as executor:
             tasks = []
             n_layers = len(all_layers)
-            n_layers_per_task = np.ceil(n_layers / tasks_count).astype(int)
-            for i in range(tasks_count):
+            n_layers_per_task = np.ceil(n_layers / n_tasks).astype(int)
+            for i in range(n_tasks):
                 indcies = range(
                     i * n_layers_per_task,
                     np.min([(i + 1) * n_layers_per_task, n_layers]),
@@ -66,12 +75,13 @@ def psd2pngs(psd_path: str, out_dir_path: Optional[str] = None, single_process: 
                 tasks.append(executor.submit(save_some_layers,
                              psd_path_, out_dir_path_, indcies))
             [future.result() for future in tqdm(
-                concurrent.futures.as_completed(tasks), total=len(tasks), unit='process(es)')]
+                concurrent.futures.as_completed(tasks), total=len(tasks), unit=' process(es)')]
     else:
         logger.info('Using single process...')
         pbar = tqdm(all_layers, unit='file(s)')
         for layer_info in pbar:
             save_layer(psd.size, layer_info)
+            
     
-    if __name__ == '__main__':
-        psd2pngs()
+if __name__ == '__main__':
+    psd2pngs()
--- psd2pngs/layers.py
+++ psd2pngs/layers.py
@@ -8,7 +8,7 @@ def _get_safe_name(name: str):
     return name.translate(str.maketrans('*\\/:?"<>| ', '-_________'))
 
 
-class LayerInfo(TypedDict):
+class ImageLayerInfo(TypedDict):
     absolute_path: Path
     layer: PSDImage
 
@@ -20,11 +20,11 @@ def save_some_layers(psd_path: Path, out_dir_path: Path, layer_indcies: Iterable
         save_layer(psd.size, layer_infos[i])
 
 
-def search_all_layers(layer: PSDImage, current_dir: Path) -> Generator[LayerInfo, None, None]:
-    absolute_path = current_dir
+def search_all_layers(layer: PSDImage, current_absolute_path: Path) -> Generator[ImageLayerInfo, None, None]:
+    absolute_path = current_absolute_path
     if layer.kind != 'psdimage':    
         layer_safe_name = _get_safe_name(layer.name)
-        absolute_path = current_dir.joinpath(layer_safe_name)
+        absolute_path = current_absolute_path.joinpath(layer_safe_name)
         
     is_group = layer.is_group()
     if not is_group:
@@ -36,10 +36,35 @@ def search_all_layers(layer: PSDImage, current_dir: Path) -> Generator[LayerInfo
             for child_found in search_all_layers(child, absolute_path):
                 yield child_found
     else:
-        yield LayerInfo(absolute_path=absolute_path, layer=layer)
+        yield ImageLayerInfo(absolute_path=absolute_path, layer=layer)
+    
+class LayerInfo(TypedDict):
+    local_path: str
+    name: str
+    safe_name: str
+    is_visible: bool
+    is_group: bool
+    children: "Iterable[LayerInfo]"
+    
+def get_layer_info(layer: PSDImage, current_local_path: Path = Path('')) -> LayerInfo:  
+    name = layer.name
+    safe_name = _get_safe_name(name)
+    is_visible = layer.is_visible()
+    is_group = layer.is_group()
+    children = []
+    
+    local_path = current_local_path
+    
+    if layer.kind != 'psdimage':
+        local_path = current_local_path.joinpath(_get_safe_name(layer.name))
+    
+    if is_group:
+        children = [get_layer_info(child, local_path) for child in layer]
+    
+    return LayerInfo(local_path=str(local_path), name=name, safe_name=safe_name, is_visible=is_visible, is_group=is_group, children=children)
+        
 
-
-def save_layer(image_size: tuple[int, int], layer_info: LayerInfo) -> None:
+def save_layer(image_size: tuple[int, int], layer_info: ImageLayerInfo) -> None:
     with Image.new('RGBA', image_size, (0, 0, 0, 0)) as img:
         img_pil = layer_info['layer'].topil()
         if img_pil is not None:
